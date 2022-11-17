0b3f02bdefae2ea3ac523bf2f3728292d0bc9939
MANI <maniadishu7342@gmail.com>
2022-11-17 22:21:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-# Project: Eating Utensil Detector Using TensorFlow and OpenCV
-
-
-# Description: This program detects forks, spoons, and knives
- 
 import cv2 as cv # OpenCV computer vision library
 import numpy as np # Scientific computing library 
- 
-#  classes = ['person','bicycle','car','motorcycle','airplane' ,'bus','train','truck','boat' ,'traffic light','fire hydrant',
-#    'stop sign','parking meter','bench','bird','cat','dog','horse','sheep','cow','elephant','bear','zebra','giraffe' ,
-#    'backpack','umbrella','handbag' ,'tie','suitcase','frisbee' ,'skis','snowboard','sports ball' ,'kite',
-#    'baseball bat','baseball glove','skateboard','surfboard','tennis rack','bottle','wine glass','cup','fork','knife',
-#    'spoon','bowl','banana','apple' ,'sandwich','orange','broccoli','carrot','hot dog','pizza' ,'donut' ,'cake',
-#    'chair' ,'couch' ,'potted plant','bed','dining table','toilet','tv','laptop','mouse','remote','keyboard',
-#    'cell phone','microwave','oven','toaster','sink','refrigerator','book','clock','vase','scissors' ,'teddy bear',
-#    'hair drier','toothbrush']
  
 # Just use a subset of the classes
 classes = ["background", "person", "bicycle", "car", "motorcycle",
